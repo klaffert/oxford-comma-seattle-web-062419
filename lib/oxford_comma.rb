5e9524bcc5faaedb0d533d23b6d75ca167e6b04e
Kelliann Lafferty <kell10192@gmail.com>
2019-06-06 03:28:36 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,8 @@ if array.size == 1
   array.join(" and ")
 elsif array.size == 2 
   array.join(" and ")
-else array.each  {value| puts value}
+else array.size == 3
+  array.each {|value| puts value}
 end
 end
 end 
