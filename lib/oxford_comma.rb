b200d58a5e5f15ce1c060a9249fe5437984be3ee
Kelliann Lafferty <kell10192@gmail.com>
2019-06-06 03:11:51 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,7 @@
 def oxford_comma(array)
-if array.size == 2 
+if array.size == 1 
+  array.join(" and ")
+elsif array.size == 2 
   array.join(" and ")
 else array.size == 3 
   puts "kiwi, durian, and starfruit"
