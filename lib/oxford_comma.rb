c6012ccd1256f40933c8ff0a975e9b6fc3fc9c1c
Kelliann Lafferty <kell10192@gmail.com>
2019-06-06 03:26:26 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ if array.size == 1
   array.join(" and ")
 elsif array.size == 2 
   array.join(" and ")
-else array.each do {|value| puts value}
+else array.each  {value| puts value}
 end
 end
 end 
