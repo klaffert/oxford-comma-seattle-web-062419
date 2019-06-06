ee10ed2ba1e2920aa532364a3d1c2f083d15617a
Kelliann Lafferty <kell10192@gmail.com>
2019-06-06 03:46:42 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,12 +1,8 @@
 def oxford_comma(array)
 if array.size == 1 
   array.join(", ")
-elsif array.size == 2 
+else array.size == 2 
   array.join(" and ")
-else 
-  more_fruit = []
-  array.each do {|value| puts ", "}
-  more_fruit << #{value}
-end
 end 
 end 
+
