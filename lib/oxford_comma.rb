1d2af217ac0c56e32a118e1a5951a4ba164b7ce9
Kelliann Lafferty <kell10192@gmail.com>
2019-06-06 03:54:56 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,6 @@
 def oxford_comma(array)
 if array.length <=2 
   array.join(" and ")
-else (array.first array.size - 1).join(', ') + ", and #{a.last}"
+else (array.first array.size - 1).join(', ') + ", and #{array.last}"
 end
 end 
